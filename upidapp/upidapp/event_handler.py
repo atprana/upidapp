df2a1c6cde8b3f0c8b262330bd6cb9b079ca957c
atprana <atprana@ATPLAPTOP.localdomain>
2020-05-29 22:03:44 +0700

    mei

--- upidapp/upidapp/event_handler.py
+++ upidapp/upidapp/event_handler.py
@@ -1,6 +1,5 @@
 import frappe
 
-
 def beforeSubmit(doc, method):
     if method == 'before_submit':
         if doc.owner == frappe.session.user and not 'Self Submitter' in frappe.get_roles(frappe.session.user):
