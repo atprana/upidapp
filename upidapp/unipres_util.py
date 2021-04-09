08c0df1d45363a6e743264baebea531d672b47cc
atprana <atprana@ATPLAPTOP.localdomain>
2021-04-09 10:42:02 +0700

    add unipres_util.py

---
+++ upidapp/unipres_util.py
@@ -0,0 +1,9 @@
+import frappe
+
+@frappe.whitelist()
+def get_default_warehouse(itemcode):	
+	return frappe.db.sql("""select default_warehouse from `tabItem Default` itdef where itdef.parent = %s limit 1""",itemcode )	
+	
+@frappe.whitelist()
+def get_employee(userid):
+	return frappe.db.sql("""select name from tabEmployee emp where  emp.user_id = %s""", userid)
